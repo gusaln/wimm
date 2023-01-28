49c9b76b65c0b8c4e45bee62047e909f85a68435
Gustavo López <git.gustavolopez.xyz@gmail.com>
2023-01-28 14:26:36 -0400

    Fix other fields changing after onValueChange in OutlinedDateTextField

--- common/src/desktopMain/kotlin/me/gustavolopezxyz/common/ui/components.kt
+++ common/src/desktopMain/kotlin/me/gustavolopezxyz/common/ui/components.kt
@@ -53,28 +53,24 @@ fun OutlinedDateTextField(
 ) {
     var dateString by remember { mutableStateOf(date.toString().replace("-", "/")) }
 
-    fun handleChange(raw: String) {
-        val parts = raw.filter { c -> c.isDigit() || c == '/' }.split('/')
-
-        val year = parts[0].take(4).padStart(1, '0').toInt()
-        val month = parts[1].take(2).padStart(1, '0').toInt().minMax(1, 12)
-        val dayOfMonth = parts[2].take(2).padStart(1, '0').toInt().minMax(
-            min = 1, max = when (month) {
-                2 -> 27
-                4, 6, 9, 11 -> 30
-                else -> 31
-            }
-        )
-
-        onValueChange(LocalDate(year, month, dayOfMonth))
-
-        dateString = "${year.toString().padStart(4, '0')}/${month.toString().padStart(2, '0')}/${
-            dayOfMonth.toString().padStart(2, '0')
-        }"
-    }
-
     OutlinedTextField(
-        dateString, ::handleChange, modifier = modifier, label = label, placeholder = placeholder, singleLine = true
+        dateString, onValueChange = { raw ->
+            val parts = raw.filter { c -> c.isDigit() || c == '/' }.split('/')
+
+            val year = parts[0].take(4).padStart(1, '0').toInt()
+            val month = parts[1].take(2).padStart(1, '0').toInt().minMax(1, 12)
+            val dayOfMonth = parts[2].take(2).padStart(1, '0').toInt().minMax(
+                min = 1, max = when (month) {
+                    2 -> 27
+                    4, 6, 9, 11 -> 30
+                    else -> 31
+                }
+            )
+
+            onValueChange(LocalDate(year, month, dayOfMonth))
+
+            dateString = "%04d/%02d/%02d".format(year, month, dayOfMonth)
+        }, modifier = modifier, label = label, placeholder = placeholder, singleLine = true
     )
 }
 

2be7db78d7fc8e29f243203279f1cd641866b8eb
Gustavo López <git.gustavolopez.xyz@gmail.com>
2023-01-28 00:15:47 -0400

    Add logger and implement BackupService

--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -2,9 +2,17 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import me.gustavolopezxyz.common.DesktopApp
 import me.gustavolopezxyz.common.di.initKoin
+import me.gustavolopezxyz.common.services.BackupService
+import org.koin.java.KoinJavaComponent.get
 
 fun main() = application {
-    initKoin()
+    initKoin {
+        printLogger()
+    }.koin.logger.info("Application started")
+
+    val backupService = get<BackupService>(BackupService::class.java)
+
+    backupService.run()
 
     Window(onCloseRequest = ::exitApplication, title = "WIMM - Where is my Money?") {
         DesktopApp()
